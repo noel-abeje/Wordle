5b5f32b8ffb6a63e2126f592d44c9ab216df006d
noel-abeje <noel.abeje@emu.edu>
2022-09-12 13:01:48 -0400

    Create Layout

---
+++ app/src/main/java/com/example/wordleapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.wordleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    /**
+     * Parameters / Fields:
+     *   wordToGuess : String - the target word the user is trying to guess
+     *   guess : String - what the user entered as their guess
+     *
+     * Returns a String of 'O', '+', and 'X', where:
+     *   'O' represents the right letter in the right place
+     *   '+' represents the right letter in the wrong place
+     *   'X' represents a letter not in the target word
+     */
+    private fun checkGuess(guess1: String) : String {
+        var guess = guess1.uppercase() //Makes all guesses uppercase
+        var wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
+    }
+}
